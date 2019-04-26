bca1e23c7569f7ad69c537581e012e0dcfd435d1
Jared Clark <jmjclark79@gmail.com>
2019-04-26 09:01:42 -0500

    All tests that don't use null now pass

--- ChallengesWithTestsMarkVII/ChallengesSet02.cs
+++ ChallengesWithTestsMarkVII/ChallengesSet02.cs
@@ -92,7 +92,7 @@ namespace ChallengesWithTestsMarkVII
             int sum = 0;
 
             foreach (int number in numbers)
-            {
+            {   
                 sum += number;
             }
 
