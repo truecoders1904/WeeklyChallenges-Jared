c614c2fea564f6fe094294aaa7f44de2fc91b48f
Jared Clark <jmjclark79@gmail.com>
2019-04-30 09:34:24 -0500

    Divide() now passes all tests

--- ChallengesWithTestsMarkVII/ChallengesSet03.cs
+++ ChallengesWithTestsMarkVII/ChallengesSet03.cs
@@ -77,7 +77,14 @@ namespace ChallengesWithTestsMarkVII
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if (divisor == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return dividend/divisor;
+            }
         }
 
         public int LastMinusFirst(int[] nums)
