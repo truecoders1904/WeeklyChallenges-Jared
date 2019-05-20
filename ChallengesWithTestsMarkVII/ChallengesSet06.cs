2b2bea5cd5f689531f2818e15b5dd40770d7cf86
Jared Clark <jmjclark79@gmail.com>
2019-05-20 10:33:47 -0500

    IsPrimeNumber() now passes all tests

--- ChallengesWithTestsMarkVII/ChallengesSet06.cs
+++ ChallengesWithTestsMarkVII/ChallengesSet06.cs
@@ -30,7 +30,20 @@ namespace ChallengesWithTestsMarkVII
 
         public bool IsPrimeNumber(int num)
         {
-            throw new NotImplementedException();
+            if (num < 2)
+            {
+                return false;
+            }
+
+            for (int i = 2; i < num; i++)
+            {
+                if (num%i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         public int IndexOfLastUniqueLetter(string str)
