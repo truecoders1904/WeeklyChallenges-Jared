daca4f28d2704aff098a17d9f2bcb4b5b62c17cf
Jared Clark <jmjclark79@gmail.com>
2019-05-31 09:53:46 -0500

    TicTacToe() now passes all tests. Every test in project now passes

--- ChallengesWithTestsMarkVII/ChallengesSet07.cs
+++ ChallengesWithTestsMarkVII/ChallengesSet07.cs
@@ -46,14 +46,52 @@ namespace ChallengesWithTestsMarkVII
 
         public string GetNameOfHighestParentCompany(Business business)
         {
-            // If there is Company A, whose parent is Company B, whose parent is Company C, then given Company A return Company C
-            throw new NotImplementedException();
+            string nameOfParentCompany = business.Name;
+
+            while(business.ParentCompany != null)
+            {
+                nameOfParentCompany = business.ParentCompany.Name;
+                business = business.ParentCompany;
+            }
+            return nameOfParentCompany;
         }
 
         public enum TicTacToeResult { X, O, Draw }
-        public TicTacToeResult GetTicTacToeWinner(char[,] finalBoard)
+        public TicTacToeResult GetTicTacToeWinner(char[,] board)
         {
-            throw new NotImplementedException();
+            if (checkWinner(board[0,0],board[1,1],board[2,2]) || checkWinner(board[0, 2], board[1, 1], board[2, 0]))
+            {
+                return returnWinner(board[1, 1]);
+            }
+            
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                if (checkWinner(board[i,0],board[i,1],board[i,2]))
+                {
+                    return returnWinner(board[i, 0]);
+                }
+                else if (checkWinner(board[0,i],board[1,i],board[2,i]))
+                {
+                    return returnWinner(board[0,i]);
+                }
+            }
+            return TicTacToeResult.Draw;
+        }
+        private bool checkWinner(char a, char b, char c)
+        {
+            return (a == b && b == c);
+        }
+
+        private TicTacToeResult returnWinner(char c)
+        {
+            if (c == 'X')
+            {
+                return TicTacToeResult.X;
+            }
+            else
+            {
+                return TicTacToeResult.O;
+            }
         }
 
         public bool EachArrayInJaggedArrayContainsTargetNumber(int[][] numbers, int targetNumber)
